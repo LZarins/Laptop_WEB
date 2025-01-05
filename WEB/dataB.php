1288a3484dffa5223a1c277b38b2eacfa1f045c9
LZarins <Zarins84@outlook.com>
2025-01-05 21:08:42 +0200

    Commented dataB.php

--- WEB/dataB.php
+++ WEB/dataB.php
@@ -1,11 +1,17 @@
 <?php
+// Define the database server's hostname or IP address
 $servername = "fdb29.awardspace.net";
+// Define the username for database access
 $username = "3624525_zarins";
+// Define the password for the specified database user
 $password = "****";
+// Define the name of the database to connect to
 $dbname = "3624525_zarins";
 
+// Create a new connection to the database using the MySQLi object
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check if the connection was successful
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
