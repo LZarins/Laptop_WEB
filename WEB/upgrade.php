25f79290f7b6bab9387368dbd0e9276cf798ccff
LZarins <Zarins84@outlook.com>
2025-01-05 21:25:35 +0200

    Commented upgrade.php

--- WEB/upgrade.php
+++ WEB/upgrade.php
@@ -2,17 +2,20 @@
 <head>
 <style>
 
+/* Style for the navigation bar container */  
 .josla {
-  overflow: hidden;
-  background-color: #333;
+  overflow: hidden; /* Ensures that the content does not overflow the container */
+  background-color: #333; /* Sets a dark gray background color */
 }
 
+/* Style for the links inside the navigation bar */  
 .josla a {
   float: left;
   color: #f2f2f2;
   padding: 14px 16px;
 }
-
+  
+/* Hover effect for links in the navigation bar */
 .josla a:hover {
   background-color: #0000ff;
   color: black;
@@ -22,8 +25,11 @@
 </head>
 <body>
 
+<!-- Navigation bar container -->  
 <div class="josla">
+  <!-- Navigation link to the main page with an image icon -->
   <a class="hover" href="http://zarins.atwebpages.com/WEB"><img src="https://cdn.pixabay.com/photo/2017/09/13/17/45/laptop-2746336_960_720.png" witdh="20" height="20"></a>
+  <!-- Navigation link to the wishlist page -->
   <a class="hover" href="http://zarins.atwebpages.com/WEB/wish_list.php">Velmju saraksts</a>
 </div>
 
